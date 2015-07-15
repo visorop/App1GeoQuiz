67577c29f21cd61ab5d3ec4b43c6e6e969ead0d4
Veselin Demirev <visorop@gmail.com>
2015-07-15 12:18:17 +0300

    Complete app.

--- app/src/main/java/com/visorop/app1geoquiz/activities/QuizActivity.java
+++ app/src/main/java/com/visorop/app1geoquiz/activities/QuizActivity.java
@@ -6,16 +6,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.visorop.app1geoquiz.R;
 import com.visorop.app1geoquiz.model.TrueFalse;
+
+import java.util.Random;
 
 
 public class QuizActivity extends Activity {
 
     private Button mButtonTrue;
     private Button mButtonFalse;
+    private Button mButtonNext;
+    private TextView mQuizText;
+
+    private Random random = new Random();
 
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
             new TrueFalse(R.string.question_oceans, true),
@@ -25,6 +32,8 @@ public class QuizActivity extends Activity {
             new TrueFalse(R.string.question_asia, true),
     };
 
+    private int mCurrentIndex = 0;
+
     private View.OnClickListener onClickListenerQuizActivity = new View.OnClickListener() {
 
         @Override
@@ -33,11 +42,28 @@ public class QuizActivity extends Activity {
             switch(id){
                 // add more cases for more UI element`s click events
                 case R.id.button_true :
-                    Toast.makeText(QuizActivity.this,R.string.toast_correct,Toast.LENGTH_SHORT).show();
+                    if(mQuestionBank[mCurrentIndex].isTrueQuestion() == true){
+                        Toast.makeText(QuizActivity.this,R.string.toast_correct,Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(QuizActivity.this,R.string.toast_incorrect,Toast.LENGTH_SHORT).show();
+                    }
                     break;
                 case R.id.button_false :
-                    Toast.makeText(QuizActivity.this,R.string.toast_incorrect,Toast.LENGTH_SHORT).show();
+                    if(mQuestionBank[mCurrentIndex].isTrueQuestion() == false){
+                        Toast.makeText(QuizActivity.this,R.string.toast_correct,Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(QuizActivity.this,R.string.toast_incorrect,Toast.LENGTH_SHORT).show();
+                    }
                     break;
+                case R.id.button_next : {
+                    int newCurrentIndex;
+                    do{
+                        newCurrentIndex = random.nextInt(mQuestionBank.length - 1);
+                    }while(mCurrentIndex == newCurrentIndex);
+                    mCurrentIndex = newCurrentIndex;
+                    mQuizText.setText(mQuestionBank[mCurrentIndex].getQuestion());
+                    break;
+                }
             }
         }
     };
@@ -52,6 +78,12 @@ public class QuizActivity extends Activity {
 
         mButtonFalse = (Button) this.findViewById(R.id.button_false);
         mButtonFalse.setOnClickListener(onClickListenerQuizActivity);
+
+        mButtonNext = (Button) this.findViewById(R.id.button_next);
+        mButtonNext.setOnClickListener(onClickListenerQuizActivity);
+
+        mQuizText = (TextView) this.findViewById(R.id.text_quiz);
+        mQuizText.setText(mQuestionBank[mCurrentIndex].getQuestion());
     }
 
     @Override
